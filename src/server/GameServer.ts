553264d5d17bc7fbc4e1106f11557bd2c35fd97d
brunoramps <ramps.brn@gmail.com>
2024-10-30 16:56:39 -0300

    .

--- src/server/GameServer.ts
+++ src/server/GameServer.ts
@@ -10,16 +10,14 @@ export class GameServer {
         this.io = new Server(server, { cors: { origin: "http://localhost:3000" } });
 
         this.io.on("connection", (socket) => {
-            // Obtém o playerId da query da conexão
             const playerId = socket.handshake.query.playerId as string;
 
-            // Verifica se o jogador já existe com o playerId fornecido
             if (playerId && this.players[playerId]) {
                 // Reassocia o jogador existente ao novo socket.id
                 this.players[socket.id] = this.players[playerId];
-                delete this.players[playerId]; // Remove o playerId antigo
+                delete this.players[playerId];
             } else {
-                // Cria um novo jogador se não houver playerId correspondente
+                // Cria um novo jogador se `playerId` não estiver armazenado
                 this.players[socket.id] = {
                     id: socket.id,
                     exp: 0,
@@ -34,10 +32,10 @@ export class GameServer {
             this.getPlayer(this.players[socket.id]);
             this.updatePlayers();
 
-            // Processa comandos do jogador
-            socket.on("playerCommand", (command: string) => this.handlePlayerCommand(socket, command));
+            socket.on("playerCommand", (command: string) =>
+                this.handlePlayerCommand(socket, command)
+            );
 
-            // Processa desconexão
             socket.on("disconnect", () => {
                 delete this.players[socket.id];
                 this.updatePlayers();
@@ -54,7 +52,10 @@ export class GameServer {
             showCommand = false;
         }
         if (showCommand) {
-            const message: Message = { from: this.players[socket.id].name, content: command };
+            const message: Message = {
+                from: this.players[socket.id].name,
+                content: command,
+            };
             this.io.emit("newMessage", message);
         }
     }
